feeacc9e50b19a981f0f8060cd4a03a0e0b0c0f9
Benjamin25Galaz <benj.galaz@duocuc.cl>
2024-10-18 20:31:08 -0300

    Delete ProyectoDjango directory

--- ProyectoDjango/TestDjango/core/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-#Funcion para conectar con el documento home .html
-def home(request):
-    return render(request, 'core/home.html')
-
